fc1b7c51a4b6e523d0ce67a78c7cccf0d91acb23
Antonio Cheong <acheong@student.dalat.org>
2023-08-24 17:30:03 +0800

    fix critical security flaw: hardcoded secret

--- config/constants.go
+++ config/constants.go
@@ -1,6 +1,43 @@
 package config
 
+import (
+	"crypto/rand"
+	"encoding/gob"
+	"os"
+)
+
 const DBPath = "database.db"
 const Host = "localhost:3000/ws"
 
-var Secret = []byte("secret")
+var Secret []byte
+
+// Generate a random password, hash it, and store it in the Secret variable & a file
+// Load secret.gob if it exists
+func init() {
+	if _, err := os.Stat("secret.gob"); err != nil {
+		Secret = make([]byte, 64)
+		rand.Read(Secret)
+		f, err := os.Create("secret.gob")
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		encoder := gob.NewEncoder(f)
+		err = encoder.Encode(Secret)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		f, err := os.Open("secret.gob")
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		decoder := gob.NewDecoder(f)
+		err = decoder.Decode(&Secret)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+}
